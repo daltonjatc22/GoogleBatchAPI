6a8fb494905530234576fc6918789da3342425a6
daltonjatc22 <114698602+daltonjatc22@users.noreply.github.com>
2023-06-23 13:35:18 -0600

    Refactored Generator

--- src/BatchRequestUtils/PromisedPriority.ts
+++ src/BatchRequestUtils/PromisedPriority.ts
@@ -1,22 +1,22 @@
 namespace BatchRequestUtils {
-    export class PromisedPriority {
-        private _promise: Promise<any>;
+    export class PromisedPriority<T> {
+        private _promise: Promise<T>;
         private _promiseResolve!: (value: any) => void;
         private _promiseReject!: (value: any) => void;
 
         constructor() {
             // Create Promise
-            this._promise = new Promise<unknown>((resolve, reject) => {
+            this._promise = new Promise<T>((resolve, reject) => {
                 this._promiseResolve = resolve;
                 this._promiseReject = reject;
             });
         }
 
         // Promise Handling
-        public GetResponsePromise(): Promise<any>{
+        public GetResponsePromise(): Promise<T>{
             return this._promise;
         }
-        public ResolvePostRequestPromise(value: any): void{
+        public ResolvePostRequestPromise(value: T): void{
             this._promiseResolve(value);
         }
         public RejectPostRequestPromise(reason: any): void{
